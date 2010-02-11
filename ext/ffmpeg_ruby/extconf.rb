c4cb11aa35c1f89d8488b4c029d74d9cf3217db1
David Martinez <david@hackerdude.com>
2010-02-11 09:05:34 -0800

    Make it find the ffmpeg libraries in more common places.

--- ext/ffmpeg_ruby/extconf.rb
+++ ext/ffmpeg_ruby/extconf.rb
@@ -1,10 +1,26 @@
 require 'mkmf'
-dir_config("avutil")
-dir_config("avformat")
-dir_config("avcodec")
+#require 'rubygems'
+#require 'ruby-debug'
+#Debugger.start
 
-puts "ok" if have_library("avutil", "avutil_version", "libavutil/avutil.h")
-puts "ok" if have_library("avformat", "av_register_all", "libavformat/avformat.h")
-puts "ok" if have_library("avcodec", "avcodec_init", "libavcodec/avcodec.h")
+def boom(v)
+  puts(v)
+  exit 1
+end
+
+#debugger
+if RUBY_PLATFORM == "universal-darwin10.0"
+  # TODO Set the archflags to -arch x86_64 ONLY if it's a 64-bit snow leopard machine.
+	#ENV['ARCHFLAGS'] = "-arch i386 -arch x86_64"
+	$CFLAGS.sub!("-arch x86_64", "")
+end
+unless ( have_header("libavformat/avformat.h") || 
+         find_header("libavformat/avformat.h", "/opt/local/include", "/usr/local/include", "/usr/include") ) and 
+       ( have_library("avformat", "av_register_all") or 
+		     find_library("avformat", "av_register_all", "/opt/local/lib", "/usr/local/lib", "/usr/lib") )
+	boom(<<EOL)
+You need to install or tell me where to find ffmpeg. Check the mkmf.log file to see what erred out.
+EOL
+end
 
 create_makefile("ffmpeg_ruby")
