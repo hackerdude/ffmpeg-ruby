6c4838b741c0dc622d4a75e523b3923f94641715
David Martinez <david@hackerdude.com>
2010-02-16 13:14:43 -0800

    Codec vs codec context

--- ext/ffmpeg_ruby/ffmpeg_avstream.c
+++ ext/ffmpeg_ruby/ffmpeg_avstream.c
@@ -7,6 +7,7 @@ void Init_ffmpeg_ruby_avstream(VALUE module)
 		cFFMpegAVStream = rb_define_class_under(module, "AVStream", rb_cObject);
 		rb_define_method(cFFMpegAVStream, "canonical_codec_name", AVStream_canonical_codec_name, 0);
 		rb_define_method(cFFMpegAVStream, "codec", AVStream_codec, 0);
+		rb_define_method(cFFMpegAVStream, "codec_context", AVStream_codec_context, 0);
 }
 
 void AVStream_free(void *v) {}
@@ -33,6 +34,17 @@ VALUE AVStream_codec(VALUE self)
 		return Qnil;
 	}
 	VALUE rCodec  = Data_Wrap_Struct(cFFMpegAVCodec, AVCodec_mark, AVCodec_free, c);
+	rb_obj_call_init(rCodec,0,0);
+	return rCodec;
+}
+
+VALUE AVStream_codec_context(VALUE self)
+{
+	AVStream *pStream;
+	AVCodecContext *c;
+	Data_Get_Struct(self, AVStream, pStream);
+	c = pStream->codec;
+	VALUE rCodec  = Data_Wrap_Struct(cFFMpegAVCodecContext, AVCodecContext_mark, AVCodecContext_free, c);
 	rb_obj_call_init(rCodec,0,0);
 	return rCodec;
 }
--- ext/ffmpeg_ruby/ffmpeg_avstream.h
+++ ext/ffmpeg_ruby/ffmpeg_avstream.h
@@ -7,10 +7,14 @@ void AVStream_mark(void *v);
 void AVStream_free(void *v);
 VALUE AVStream_canonical_codec_name(VALUE);
 VALUE AVStream_codec(VALUE);
+VALUE AVStream_codec_context(VALUE);
 
 
 // forward declaring some stuff from codec module
 void AVCodec_mark(void *v);
 void AVCodec_free(void *v);
+void AVCodecContext_mark(void *v);
+void AVCodecContext_free(void *v);
 extern VALUE cFFMpegAVCodec;
+extern VALUE cFFMpegAVCodecContext;
 VALUE avcodec_canonical_name(AVCodecContext *enc);
